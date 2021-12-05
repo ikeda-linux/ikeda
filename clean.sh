fb337822a80ad04f66ac654376daca102e409a93
Matt C <mdc028@bucknell.edu>
2021-12-05 00:00:03 -0500

    error handling

--- clean.sh
+++ clean.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+
 rm -rf build/
 cp -r src build
 cp as_root.sh build/.
